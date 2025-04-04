3243226151b02658e7a828654a0d23eb327c457f
bensonc27 <bensonc27@nycstudents.net>
2025-04-04 12:32:02 -0400

    login

---
+++ app/src/components/Login.vue
@@ -0,0 +1,35 @@
+<template>
+    <div class="sign-in">
+        <form class="form">
+      <label class="" for="email">Email:</label>
+      <input type="text" name="email" placeholder="Email">
+
+      <label class="" for="password">Password:</label>
+      <input type="text" name="password" placeholder="Password">
+
+      <input type="submit" id="button" value="submit">
+    </form>
+    </div>
+    <div class="sign-up">
+        <form class="form">
+      <label class="" for="username">Username:</label>
+      <input type="text" name="username" placeholder="Username">
+
+      <label class="" for="age">Age:</label>
+      <input type="number" name="age" placeholder="Age">
+
+      <label class="" for="email">Email:</label>
+      <input type="text"  name="email" placeholder="Email">
+
+      <input type="submit" id="button" value="submit">
+    </form>
+    </div>
+</template>
+
+<script setup>
+
+</script>
+
+<style lang="" scoped>
+
+</style>
--- supabase.js
+++ supabase.js
@@ -1,6 +1,8 @@
 import { createClient } from "@supabase/supabase-js";
 
 const supabase = createClient(
-  "https://xyzcompany.supabase.co",
-  "public-anon-key"
+  "https://rqqucecxsqzvbzpkxgzd.supabase.co",
+  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InJxcXVjZWN4c3F6dmJ6cGt4Z3pkIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDM3ODI4MTMsImV4cCI6MjA1OTM1ODgxM30.zTJxEjXZgzvM2jc5VJTrhUeFNBFQdEK3ZpH90jSSscg"
 );
+
+export default supabase;
