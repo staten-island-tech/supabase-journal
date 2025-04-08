ad84eb1f2df5e0397e8384eb449d071a15842253
Selina227 <selinac33@nycstudents.net>
2025-04-08 12:31:00 -0400

    e

---
+++ app/src/components/Entries.vue
@@ -0,0 +1,31 @@
+<template>
+  <div>
+    <h1 class="head">My Journal</h1>
+    <form @submit.prevent="addEntry">
+      <textarea v-model="newEntry" placeholder="Write new entry here..."></textarea>
+      <button type="submit">Add entry</button>
+    </form>
+
+    <h2>Entries</h2>
+    <ul>
+      <li v-for="(entry, index) in journalEntries" :key="index" class="indiv-entry">
+        {{ entry }}
+      </li>
+    </ul>
+  </div>
+</template>
+
+<script setup>
+import { ref } from 'vue'
+const newEntry = ref('')
+const journalEntries = ref('')
+
+function addEntry() {
+  if (newEntry.value.trim()) {
+    journalEntries.value.push(newEntry.value.trim())
+    newEntry.value = ''
+  }
+}
+</script>
+
+<style scoped></style>
--- app/src/views/HomeView.vue
+++ app/src/views/HomeView.vue
@@ -1,9 +1,9 @@
 <script setup>
-import TheWelcome from '../components/TheWelcome.vue'
+import Entries from '../components/Entries.vue'
 </script>
 
 <template>
   <main>
-    <TheWelcome />
+    <Entries />
   </main>
 </template>
