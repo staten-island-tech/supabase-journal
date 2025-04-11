b7c34ff58cdee29f5f48be9f093f8d525871d6ea
bensonc27 <bensonc27@nycstudents.net>
2025-04-11 12:26:18 -0400

    Update LoginView.vue

--- app/src/views/LoginView.vue
+++ app/src/views/LoginView.vue
@@ -1,43 +1,32 @@
 <template>
-  <div>
-    <h1>Login</h1>
-    <div v-if="auth.user">
-      <p>Welcome, {{ auth.user.email }}</p>
-      <button @click="auth.signOut">Sign Out</button>
-    </div>
-
-    <div v-else>
-      <input v-model="email" type="email" placeholder="Email" />
-      <input v-model="password" type="password" placeholder="Password" />
-      <button @click="handleSignIn">Sign In</button>
-    </div>
-
-    <form class="form">
-      <label class="" for="email">Email:</label>
-      <input type="text" name="email" placeholder="Email" />
-
-      <label class="" for="password">Password:</label>
-      <input type="text" name="password" placeholder="Password" />
-
-      <input type="submit" id="button" value="submit" />
-    </form>
+  <h1>Login</h1>
+  <div v-if="auth.user">
+    <p>Welcome, {{ auth.user.email }}</p>
+    <button @click="auth.signOut">Sign Out</button>
   </div>
-  <div class="sign-up">
-    <form class="form">
-      <label class="" for="username">Username:</label>
-      <input type="text" name="username" placeholder="Username" />
 
-      <label class="" for="age">Age:</label>
-      <input type="number" name="age" placeholder="Age" />
-
-      <label class="" for="email">Email:</label>
-      <input type="text" name="email" placeholder="Email" />
-
-      <input type="submit" id="button" value="submit" />
-    </form>
+  <div v-else>
+    <input v-model="email" type="email" placeholder="Email" />
+    <input v-model="password" type="password" placeholder="Password" />
+    <button @click="handleSignIn">Sign In</button>
   </div>
 </template>
 
-<script setup></script>
+<script setup>
+import { useAuthStore } from '@/stores/auth'
+import { ref } from 'vue'
+
+const auth = ref(useAuthStore)
+const email = ref('')
+const password = ref('')
+
+const login = async () => {
+  try {
+    await auth.signInWithEmail(email.value, password.value)
+  } catch {
+    console.log(error.message)
+  }
+}
+</script>
 
 <style lang="" scoped></style>
