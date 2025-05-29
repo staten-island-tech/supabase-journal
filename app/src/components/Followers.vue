85d75caca28cfe455e9bbed91393031762102585
bensonc27 <bensonc27@nycstudents.net>
2025-05-29 12:28:55 -0400

    Adding Followers

---
+++ app/src/components/Followers.vue
@@ -0,0 +1,21 @@
+<template>
+  <div>
+    <h2>Users to Follow</h2>
+    <div v-for="user in users" :key="user.id"></div>
+  </div>
+</template>
+
+<script>
+import { SupabaseClient } from '@supabase/supabase-js'
+
+const currentUser = await supabase.auth.admin.getUserById(1)
+
+const { data: allUsers } = await supabase.from('users').select('id, email').neq('id', currentUser)
+
+const { data: following } = await supabase
+  .from('follows')
+  .select('followee_id')
+  .eq('follower_id', currentUser)
+</script>
+
+<style lang="scss" scoped></style>
