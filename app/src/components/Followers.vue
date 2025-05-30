6696d5482fe0753b9d351c06af64c2a8827bd996
bensonc27 <bensonc27@nycstudents.net>
2025-05-30 12:56:40 -0400

    follower

--- app/src/components/Followers.vue
+++ app/src/components/Followers.vue
@@ -2,6 +2,11 @@
   <div>
     <h2>Users to Follow</h2>
     <div v-for="user in users" :key="user.id"></div>
+<<<<<<< Updated upstream
+=======
+    <button @click="follow(user.id)">follow</button>
+    {{ isFollowing(user.id) ? 'Unfollow' : 'Follow' }}
+>>>>>>> Stashed changes
   </div>
 </template>
 
@@ -9,6 +14,8 @@
 import { SupabaseClient } from '@supabase/supabase-js'
 
 const currentUser = await supabase.auth.admin.getUserById(1)
+const users =
+const followedusers=
 
 const { data: allUsers } = await supabase.from('users').select('id, email').neq('id', currentUser)
 
@@ -16,6 +23,8 @@ const { data: following } = await supabase
   .from('follows')
   .select('followee_id')
   .eq('follower_id', currentUser)
+
+function follow() {}
 </script>
 
 <style lang="scss" scoped></style>
