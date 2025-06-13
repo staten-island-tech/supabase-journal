37c85700b1303f58834467962a21496bdb9a1fae
bensonc27 <bensonc27@nycstudents.net>
2025-06-12 22:45:14 -0400

    fixestoerrors

--- app/src/components/Calendar.vue
+++ app/src/components/Calendar.vue
@@ -10,7 +10,6 @@ import FullCalendar from '@fullcalendar/vue3'
 import dayGridPlugin from '@fullcalendar/daygrid'
 import timeGridPlugin from '@fullcalendar/timegrid'
 import interactionPlugin from '@fullcalendar/interaction'
-import { INITIAL_EVENTS, createEventId } from '../event-utils'
 
 const currentEvents = ref([])
 
